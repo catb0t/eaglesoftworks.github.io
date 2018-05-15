159134428336acd2958a29a1558f284a91d2aca4
Cat Stevens <catb0t@protonmail.ch>
2018-05-14 22:47:14 -0400

    update path for player frame

--- src/script/parts/sieged.vue
+++ src/script/parts/sieged.vue
@@ -6,7 +6,7 @@
     <!-- might need to use CORSBuster / SOPBuster -->
     <div>
       <!-- https://github.com/wmhilton/cors-buster -->
-      <iframe id="game-viewer" src="https://cors-buster-tbgktfqyku.now.sh/raw.githubusercontent.com/eaglesoftworks/sieged/master/Build/WebGLBuild/index.html" >
+      <iframe id="game-viewer" src="https://cors-buster-tbgktfqyku.now.sh/raw.githubusercontent.com/eaglesoftworks/sieged/master/WebGLBuild/index.html" >
       </iframe>
     </div>
   </div>
